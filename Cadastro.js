0efef25a18efd04d42f2196be6d1245cd03f520f
Luis Otávio Beckman <123770533+LuisOtavioBeckaman@users.noreply.github.com>
2023-06-16 16:21:50 -0300

    Delete Cadastro.js

--- Cadastro.js
+++
@@ -1,9 +0,0 @@
-let idade;
-
-idade = prompt("Esta página é proibida para pessoas menores de 18 anos, confirme sua idade.");
-if (idade>=18){
-    alert("autorizado")
-}else{
-    alert("não autorizado,  ");
-    window.close();
-}
